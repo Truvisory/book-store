3ad7619218de66f28e22ea3b9ee795b15b37d45a
TONY ADAMS <truvisory@TONYs-MacBook-Pro.local>
2018-12-27 17:54:50 -0700

    Initial push with Header and Modal Cart

---
+++ src/components/cartmodal.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const CartModal = () => {
+  return (
+    <div className="modal fade" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog" role="document">
+            <div className="modal-content">
+                <div className="modal-header">
+                    <h5 className="modal-title" id="exampleModalLabel">Shopping Cart</h5>
+                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div className="modal-body row">
+                    <p className="col-3">stuff</p>
+                    <p className="col-3">stuff</p>
+                </div>
+                <div className="modal-footer">
+                    <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" className="btn btn-primary">Purchase</button>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default CartModal
+
+
+
+
+
---
+++ src/components/navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <nav className="navbar bg-dark">
+        <a className="navbar-brand text-light" href="/#">Book Store</a>
+        <div className="input-group container col-sm">
+            <input type="search" className="form-control" aria-label="Text input with segmented dropdown button" placeholder="Find Books"/>
+            <div className="input-group-append">
+                <button type="button" className="btn btn-secondary">Search</button>
+                <button type="button" className="btn btn-primary dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <span className="sr-only">Toggle Dropdown</span>
+                </button>
+                <div className="dropdown-menu text-center" x-placement="bottom-start" style={{right: '0px'}}>
+                    <a className="dropdown-item" href="/#">Arrange List by Author</a>
+                    <a className="dropdown-item" href="/#">Arrange List by Title</a>
+                </div>
+            </div>
+            <button type="button" className="btn btn-outline-light" data-toggle="modal" data-target="#exampleModal" style={{marginLeft: '5px'}}>Cart <span className="badge badge-light">4</span></button>
+        </div>
+    </nav>
+  )
+}
+
+export default NavBar
